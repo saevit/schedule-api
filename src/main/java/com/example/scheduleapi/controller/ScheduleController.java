91f859bb366083b4039aafedea10c037bd3aba89
saevit <woosaevit@gmail.com>
2025-05-14 12:32:34 +0900

    Lv 4. 페이지네이션을 적용한 일정 조회 기능 구현(주소 수정)

--- src/main/java/com/example/scheduleapi/controller/ScheduleController.java
+++ src/main/java/com/example/scheduleapi/controller/ScheduleController.java
@@ -29,7 +29,7 @@ public class ScheduleController {
     }
 
     // 전체 일정 조회 (페이지네이션)
-    @GetMapping("/page")
+    @GetMapping
     public ResponseEntity<List<ScheduleResponseDto>> findSchedule(@RequestParam int page,
                                                                   @RequestParam int size,
                                                                   @RequestParam(required = false) Long authorId,
